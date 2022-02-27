9da8b8b99dda34a6af2689b7c5a9a83304ee8e49
kxrxll <kovalyovkirill@gmail.com>
2022-02-27 16:08:55 +0300

    basic local search

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Banner from './Banner';
 import CatalogItems from './CatalogItems';
+import { catalogSuccess } from '../actions';
 
 function Catalog() {
+  const {items} = useSelector(state => state.catalogItems);
+  const dispatch = useDispatch();
+
+  const handleInput = (evt) => {
+    const search = evt.target.value;
+    const newItems = items.filter(item => {
+      console.log(item);
+      return item.title.startsWith(search);
+    });
+    console.log(newItems);
+    dispatch(catalogSuccess(newItems))
+  }
+
   return (
     <main className="container">
       <div className="row">
@@ -11,7 +27,7 @@ function Catalog() {
           <section className="catalog">
             <h2 className="text-center">Каталог</h2>
             <form className="catalog-search-form form-inline">
-              <input className="form-control" placeholder="Поиск" />
+              <input className="form-control" placeholder="Поиск" onChange={handleInput}/>
             </form>
             <CatalogItems />
           </section>
